523a69e9e3770b5c3a06fdf583383bd91f576ed5
RTdev <77839017+R1TGAMING@users.noreply.github.com>
2024-05-09 14:28:16 +0000

    html css improvement

--- main.py
+++ main.py
@@ -12,17 +12,20 @@ import youtubeuser
 from PIL import Image, ImageDraw, ImageFont
 from io import BytesIO
 from fastapi.responses import FileResponse, StreamingResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
-templates = Jinja2Templates(directory="main_web")
+templates = Jinja2Templates(directory="./main_web")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 ##MAIN PAGE
-@app.get("/api", response_class=HTMLResponse)
+@app.get("/api")
 def main_api(request : Request) :
+  
   return templates.TemplateResponse("index.html", {"request": request})
 
 ##GITHUB USER INFORMATION
-@app.get("/api/githubuser&&name={name}")
+@app.get("/api/githubUser&&name={name}")
 def read_root(name : str):
   info = userGithub.get(name)
   
@@ -63,6 +66,7 @@ async def read_root(text : str) :
   bytes.seek(0)
   return StreamingResponse(BytesIO(bytes.read()), media_type="image/png")
 
+## DRAKE MEME API
 @app.get("/api/drakeMeme&&text1={text1}&text2={text2}")
 async def read_root(text1 : str, text2 : str) :
   openimg = Image.open("./images/drakeMeme.jpeg")
@@ -76,6 +80,8 @@ async def read_root(text1 : str, text2 : str) :
   openimg.save(bytes, format="PNG")
   bytes.seek(0)
   return StreamingResponse(BytesIO(bytes.read()), media_type="image/png")
-  
+
+
+
 if __name__ == "__main__":
     uvicorn.run(app, host='127.0.0.1', port = 8000)
